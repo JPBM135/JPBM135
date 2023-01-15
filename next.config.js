529a1d6fce7109fc226e97f3c844657c2ad9a463
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-01-15 04:21:29 -0300

    chore(next): config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ['cdn.discordapp.com'],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
