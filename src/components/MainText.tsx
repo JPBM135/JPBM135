28cd3ebae5ee80713aa78ada6958dae41ef7d7f9
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-01-18 21:13:28 -0300

    feat(svg): change on hover

--- src/components/MainText.tsx
+++ src/components/MainText.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
+import { GithubLogo, Envelope, TwitterLogo } from 'phosphor-react';
 import { SOCIAL_ICON_SIZE } from '@/constants';
+
+function changeColorToTeal(evt: React.MouseEvent) {
+	// @ts-expect-error: Fuck you
+	evt.currentTarget.style.color = '#06B6D4';
+}
 
 export default function MainText() {
 	return (
@@ -33,34 +39,16 @@ export default function MainText() {
 					<span className="text-zinc-200 hover:text-teal-50">r</span>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center pt-7" id="social-icons">
+			<div className="flex flex-col justify-center items-center pt-5" id="social-icons">
 				<div className="flex flex-row justify-center items-center space-x-6" id="social-icons">
-					<a href="https://github.com/JPBM135">
-						<Image
-							alt="Github Icon"
-							className="rounded-lg block mx-auto shadow-current"
-							height={SOCIAL_ICON_SIZE}
-							src="/github.svg"
-							width={SOCIAL_ICON_SIZE}
-						/>
+					<a href="https://github.com/JPBM135" rel="noreferrer" target="_blank">
+						<GithubLogo className="svg-social-icons" size={SOCIAL_ICON_SIZE} width="bold" />
 					</a>
-					<a href="https://twitter.com/JPBM135">
-						<Image
-							alt="Twitter Icon"
-							className="rounded-lg block mx-auto shadow-current"
-							height={SOCIAL_ICON_SIZE}
-							src="/twitter.svg"
-							width={SOCIAL_ICON_SIZE}
-						/>
+					<a href="https://twitter.com/JPBM135" rel="noreferrer" target="_blank">
+						<TwitterLogo className="svg-social-icons" size={SOCIAL_ICON_SIZE} width="bold" />
 					</a>
-					<a href="mailto:jpedrobm0@gmail.com">
-						<Image
-							alt="Email Icon"
-							className="rounded-lg block mx-auto shadow-current"
-							height={SOCIAL_ICON_SIZE}
-							src="/email.svg"
-							width={SOCIAL_ICON_SIZE}
-						/>
+					<a href="mailto:jpedrobm0@gmail.com" rel="noreferrer" target="_blank">
+						<Envelope className="svg-social-icons" size={SOCIAL_ICON_SIZE} width="bold" />
 					</a>
 				</div>
 			</div>
