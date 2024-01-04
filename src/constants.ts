6d157d84ff4242ef47e6e42478db05714a8f2654
JPBM135 <jpedrobm0@gmail.com>
2024-01-03 19:55:34 -0800

    feat(head): improve seo meta tags

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
 export const SOCIAL_ICON_SIZE = '50';
+export const SITE_URL = 'https://jpbm.dev';
+export const META_TITLE = 'JPBM135 - Full-Stack Developer';
+export const META_DESCRIPTION = 'Passionate and self-driven full-stack developer known for their energetic approach to coding. A dedicated learner, they infuse creativity and innovation into every project, making the development process both productive and enjoyable.'
+export const META_IMAGE = `${SITE_URL}/metadata_cover.png`;
